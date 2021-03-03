46e0fe32607028146a8917f7c9d9cf7709afdb01
Sachin <sachin.pampannavar@goalsr.com>
2021-03-03 13:24:43 +0530

    first commit

---
+++ WeatherApp/WeatherApp/App/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  WeatherApp
+//
+//  Created by Sachin on 03/03/21.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
